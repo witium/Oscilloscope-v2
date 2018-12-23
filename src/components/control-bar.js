87083120fcdcf8f3214224ca93b1ae22f018245a
mhrice <matthewrice11@yahoo.com>
2018-12-22 20:48:18 -0500

    Ipadtest28

--- src/components/control-bar.js
+++ src/components/control-bar.js
@@ -555,9 +555,9 @@ export default class ControlBar extends Component {
               freq = this.prevFreq[e.touches[i].identifier];
             }
               this.goldIndices.splice(index, 1);
+              this.synths[index].volume.cancelScheduledValues(0);
               this.synths[index].triggerRelease();
               console.log("RELEASE")
-              this.synths[index].volume.cancelScheduledValues(0);
               this.synths[index].volume.linearRampToValueAtTime(-100, this.props.context.currentTime+2);
 
               this.label(freq, pos.x, pos.y, index );
